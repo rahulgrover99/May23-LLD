ff6cd8d364a3debe1782e57097beb4d2cbfa2179
rahulgrover99 <rahulgrover99@gmail.com>
2023-05-17 22:20:41 +0100

    Add second lecture notes.

--- code/IntroToLLD/src/accessmodifier/Client.java
+++ code/IntroToLLD/src/accessmodifier/Client.java
@@ -2,8 +2,12 @@ package accessmodifier;
 
 public class Client {
     public static void main(String[] args) {
+        System.out.println(Box.time_pass);
+        Box.time_pass += 10;
+        System.out.println(Box.time_pass);
         Box.printMe();
         Box b1 = new Box();
         System.out.println(b1.getWidth());
+        System.out.println(b1.time_pass);
     }
 }
