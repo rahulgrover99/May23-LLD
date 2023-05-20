45861e075ae65c738b38f8da89c833abc4ce5651
rahulgrover99 <rahulgrover99@gmail.com>
2023-05-20 02:22:56 +0100

    Test things.

---
+++ contrib/rahul-grover.md
@@ -0,0 +1,2 @@
+Rahul Grover
+Foobar batch
---
+++ solutions/rahulgrover99/Main.java
@@ -0,0 +1,7 @@
+package solutions.rahulgrover99;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Teach me coding senpai!");
+    }
+}
