2552fc50672779092fd3c328e1d3bf13901590c7
rahulgrover99 <rahulgrover99@gmail.com>
2023-05-21 22:10:08 +0100

    Fix resource leak

--- solutions/nikhillund/Main.java
+++ solutions/nikhillund/Main.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-        int h = sc.nextInt();
-        int w = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            String input = sc.next();
+            int h = sc.nextInt();
+            int w = sc.nextInt();
 
-        Shape shape;
-        if (input.equalsIgnoreCase("Triangle")) {
-            shape = new Triangle(h, w);
-        } else {
-            shape = new Rectangle(h, w);
+            Shape shape;
+            if (input.equalsIgnoreCase("Triangle")) {
+                shape = new Triangle(h, w);
+            } else {
+                shape = new Rectangle(h, w);
+            }
+            printArea(shape);
         }
-        printArea(shape);
     }
 
     private static void printArea(Shape shape) {
