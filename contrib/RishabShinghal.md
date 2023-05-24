c9ba119a2479729cdb360367ded484b655f54347
Rishab Shinghal <40848772+rish8089@users.noreply.github.com>
2023-05-24 21:47:00 +0530

    Assignment 1 Part 3 & 4 (#17)
    * Create RishabShinghal.md
    
    * Update RishabShinghal.md
    
    * Update RishabShinghal.md
    
    * Part 4
    
    ---------
    
    Co-authored-by: Rishab <rishab@Rishabs-MacBook-Pro.local>

---
+++ contrib/RishabShinghal.md
@@ -0,0 +1,7 @@
+Name :- Rishab Shinghal
+
+Batch :- August 2022
+
+Part 1 Git repo :- https://github.com/rish8089/ScalerLLDAssignments
+
+Part 2 Git repo :- https://github.com/rish8089/ScalerLLDAssignment1Part2
---
+++ solutions/rish8089/Shape.java
@@ -0,0 +1,13 @@
+package solutions.rish8089;
+
+public class Shape {
+    double height;
+    double width;
+    Shape(double height, double width){
+        this.height=height;
+        this.width=width;
+    }
+    public double compute_area(){
+        return 0;
+    }
+}
