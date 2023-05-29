4c45812e62d4711d38c80523a52bfe9ce304d59f
Gautham S <gauthamsundarrajan@outlook.com>
2023-05-30 02:01:12 +0530

    Solution to Assignment 1, Part 4 (#37)

---
+++ solutions/gauthams89/Shape.java
@@ -0,0 +1,16 @@
+package solutions.gauthams89;
+
+public class Shape {
+	double height;
+	double width;
+	
+	public Shape(double height, double width) {
+		super();
+		this.height = height;
+		this.width = width;
+	}
+	
+	public double compute_area() {
+		return 0;
+	}
+}
