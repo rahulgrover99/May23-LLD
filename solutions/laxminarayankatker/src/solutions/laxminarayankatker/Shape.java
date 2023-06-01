666277e1b890be72e8708c739db39305bb288b6d
CREATIVECAPSULE\laxminarayan katker <laxminarayan.katker@creativecapsule.com>
2023-06-01 11:20:55 +0530

    Added code to respective shape files

--- solutions/laxminarayankatker/src/solutions/laxminarayankatker/Shape.java
+++ solutions/laxminarayankatker/src/solutions/laxminarayankatker/Shape.java
@@ -1,2 +1,20 @@
-package solutions.laxminarayankatker;public class Shape {
+package solutions.laxminarayankatker;
+
+public class Shape {
+     double height;
+      double width;
+    public Shape()
+     {
+        this.height=0;
+        this.width=0;
+     }
+     public Shape(double height,double width)
+     {
+         this.height = height;
+         this.width = width;
+     }
+     public double compute_area()
+     {
+         return 0;
+     }
 }
--- solutions/laxminarayankatker/src/solutions/laxminarayankatker/Triangle.java
+++ solutions/laxminarayankatker/src/solutions/laxminarayankatker/Triangle.java
@@ -1,2 +1,16 @@
-package solutions.laxminarayankatker;public class Triangle {
+package solutions.laxminarayankatker;
+
+public class Triangle  extends Shape {
+
+    public Triangle() {
+        super();
+    }
+    public Triangle(double height,double width) {
+        super(height, width);
+    }
+    public double compute_area()
+    {
+        return 0.5*(this.height*this.width);
+    }
+
 }
